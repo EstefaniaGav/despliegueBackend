69e2641182c2321acda0a6c658d1b29fdce34e80
EstefaniaGav <estefania.gaviria91@gmail.com>
2023-12-17 12:28:25 -0500

    origin

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(cors({
     origin: 'https://demeter-front-production.up.railway.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    // credentials: true
 }));
 
 app.use(morgan('dev'));
