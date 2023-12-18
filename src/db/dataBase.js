5a1a0e411018e75a82b1cc602d5a17a14b5517e0
EstefaniaGav <estefania.gaviria91@gmail.com>
2023-12-17 22:42:30 -0500

    sql_mode active

--- src/db/dataBase.js
+++ src/db/dataBase.js
@@ -1,17 +1,20 @@
 import { Sequelize } from 'sequelize';
 
-
 const dbConfig = {
   host: process.env.BD_HOST || "localhost",
   port: process.env.BD_PORT || "3306",
   username: process.env.BD_USER || 'root',
   password: process.env.BD_PASSWORD || "",
   database: process.env.BD_NAME || "demeterfinal",
-  dialect: 'mysql' 
-  
+  dialect: 'mysql'
+
 }
 
-export const sequelize = new Sequelize(
-  
-  dbConfig,
-);
+export const sequelize = new Sequelize({
+  ...dbConfig,
+  dialectOptions: {
+    options: {
+      sql_mode: "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+    }
+  }
+});
