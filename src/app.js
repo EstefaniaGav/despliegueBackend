cd055b51c344812d033a01bd56fcc552cd77d1fb
EstefaniaGav <estefania.gaviria91@gmail.com>
2023-12-18 00:16:07 -0500

    new env added

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ import moduleRoutes from './routes/module.routes.js';
 const app = express();
 
 app.use(cors({
-    origin: 'https://demeter-front-production.up.railway.app',
+    origin: process.env?.FRONT_URL || 'https://desplieguedemeter-production.up.railway.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
