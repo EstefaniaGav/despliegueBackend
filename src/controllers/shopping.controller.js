b44a7cd476e8bd49657a324a26bccb8114a329e2
EstefaniaGav <estefania.gaviria91@gmail.com>
2023-12-18 22:38:34 -0500

    group

--- src/controllers/shopping.controller.js
+++ src/controllers/shopping.controller.js
@@ -146,21 +146,13 @@ export const getShoppingAndSuppliesBySupplierIdAndDate = async (req, res) => {
 export const getShopingByProvider = async (req, res) => {
     try {
 
-        // await sequelize.query("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
         const shoppingBySupplier = await shopping.findAll({
-            attributes: ['Datetime'],
             include: [{
                 model: supplier,
                 required: true
             }],
-            group: ['Datetime']
+            // group: "Datetime"
         })
-        //     include: [{
-        //         model: supplier,
-        //         required: true
-        //     }],
-        //     group: "Datetime"
-        // })
 
         res.json(shoppingBySupplier);
     } catch (error) {
